91cfd40583807ee8e808b6b9799253a43b3bbb60
sippitechnologies <sippitechnologies@gmail.com>
2021-02-11 21:39:59 +0530

    Done with Trip Calculator App

--- fundamentalflutter/helloworldapp/lib/apps/tripcostcalculator/tipcalcualorhome.dart
+++ fundamentalflutter/helloworldapp/lib/apps/tripcostcalculator/tipcalcualorhome.dart
@@ -15,65 +15,92 @@ class _TripCostHomeState extends State<TripCostHome> {
   var ratecontroller = TextEditingController();
   Widget build(BuildContext buildContext) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Trip Cost'),
-      ),
-      body: Column(
-        children: [
-          TextField(
-            controller: distancecontroller,
-            keyboardType: TextInputType.number,
-            decoration:
-                InputDecoration(labelText: "Distance", hintText: "eg 14"),
-          ),
-          TextField(
-            controller: consumptioncontroller,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Consumption Rate", hintText: "eg 14"),
-          ),
-          TextField(
-            controller: ratecontroller,
-            keyboardType: TextInputType.number,
-            decoration:
-                InputDecoration(labelText: "Fule Price", hintText: "eg 14"),
-          ),
-          DropdownButton(
-            value: currency,
-            onChanged: (value) {
-              setState(() {
-                currency = value;
-              });
-            },
-            items: _currencies.map<DropdownMenuItem<String>>(
-              (String value) {
-                return DropdownMenuItem(
-                  child: Text(value),
-                  value: value,
-                );
-              },
-            ).toList(),
-          ),
-          Text(calculatedPrice),
-          Row(
-            children: [
-              Expanded(
-                child: RaisedButton(
-                  child: Text('Calculate'),
-                  onPressed: calCulate,
+        appBar: AppBar(
+          title: Text('Trip Cost'),
+        ),
+        body: (Container(
+            padding: EdgeInsets.only(left: 16, right: 16),
+            child: Column(
+              children: [
+                Padding(
+                  child: TextField(
+                    controller: distancecontroller,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Distance",
+                        hintText: "eg 14",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0))),
+                  ),
+                  padding: EdgeInsets.only(top: 8),
                 ),
-              ),
-              Expanded(
-                child: RaisedButton(
-                  child: Text('Reset'),
-                  onPressed: reset,
+                Padding(
+                  child: TextField(
+                    controller: consumptioncontroller,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Consumption Rate",
+                        hintText: "eg 14",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0))),
+                  ),
+                  padding: EdgeInsets.only(top: 8),
                 ),
-              )
-            ],
-          )
-        ],
-      ),
-    );
+                Padding(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: ratecontroller,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                              labelText: "Fule Price",
+                              hintText: "eg 14",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0))),
+                        ),
+                      ),
+                      Expanded(
+                        child: DropdownButton(
+                          value: currency,
+                          onChanged: (value) {
+                            setState(() {
+                              currency = value;
+                            });
+                          },
+                          items: _currencies.map<DropdownMenuItem<String>>(
+                            (String value) {
+                              return DropdownMenuItem(
+                                child: Text(value),
+                                value: value,
+                              );
+                            },
+                          ).toList(),
+                        ),
+                      )
+                    ],
+                  ),
+                  padding: EdgeInsets.only(top: 8),
+                ),
+                Text(calculatedPrice),
+                Row(
+                  children: [
+                    Expanded(
+                      child: RaisedButton(
+                        child: Text('Calculate'),
+                        onPressed: calCulate,
+                      ),
+                    ),
+                    Expanded(
+                      child: RaisedButton(
+                        child: Text('Reset'),
+                        onPressed: reset,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ))));
   }
 
   void calCulate() {
@@ -92,6 +119,7 @@ class _TripCostHomeState extends State<TripCostHome> {
       ratecontroller.text = "";
       consumptioncontroller.text = "";
       currency = 'Dollars';
+      calculatedPrice = '';
     });
   }
 }
